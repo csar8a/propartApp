18cefa34f0416ee7363b2b11d3a5c197d84e85f5
cesar <cesar.ochoa@urp.edu.pe>
2020-05-07 20:42:48 -0500

    create 2 communication class

--- app/src/main/java/com/ingwilson/migranparte2/comunicacion2.java
+++ app/src/main/java/com/ingwilson/migranparte2/comunicacion2.java
@@ -3,12 +3,26 @@ package com.ingwilson.migranparte2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class comunicacion2 extends AppCompatActivity {
-
+public class comunicacion2  extends AppCompatActivity {
+    TextView txtmensaje;
+    String nombre;
+    int edad;
+    int evaluacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comunicacion2);
+        Bundle extras = getIntent().getExtras();
+        nombre= extras.getString("nombre");
+        edad = extras.getInt("edad");
+        txtmensaje=findViewById(R.id.txtmensaje);
+        if (edad > 25)
+            evaluacion = 3000;
+        else
+            evaluacion = 2000;
+        txtmensaje.setText("Buen día "+ nombre + " por su edad de " + edad +
+                " podemos ofrecerle " + evaluacion + "soles");
     }
 }
