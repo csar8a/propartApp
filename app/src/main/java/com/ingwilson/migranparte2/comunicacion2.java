7133e5ddf30dc70b6cb1e1ec15cc9fca2c48a842
cesar <cesar.ochoa@urp.edu.pe>
2020-05-07 20:37:48 -0500

    sincro proyect

---
+++ app/src/main/java/com/ingwilson/migranparte2/comunicacion2.java
@@ -0,0 +1,14 @@
+package com.ingwilson.migranparte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class comunicacion2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comunicacion2);
+    }
+}
